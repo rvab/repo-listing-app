a227ac81e72c50053d46a1e620a716b92f6517e1
Aditya Baddur <adityabaddur@Adityas-MacBook-Air.local>
2022-03-24 11:23:40 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/repo-list'));
+app.use(express.static(__dirname + '/dist/repo-listing-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/repo-list/index.html'));});
+'/dist/repo-listing-app/index.html'));});
 app.listen(process.env.PORT || 8080);
