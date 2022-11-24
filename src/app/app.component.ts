083cf57a52876e1c241f865231a4159a4dcd8679
Aditya Baddur <adityabaddur@Adityas-MacBook-Air.local>
2022-11-24 11:16:14 +0530

    lint testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ import { RepoListParams } from "./components/repo-list/repo-list.component";
 export class AppComponent {
   userInfo: UserDetailsParams;
   repoLists: RepoListParams[];
-  totalRepos: number;
-  currentPage: number;
+  totalRepos: number
+  currentPage: number
   showSkeleton: boolean = false;
   direction: string = "desc"    
   errorText: string = ""  
