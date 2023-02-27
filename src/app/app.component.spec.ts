7aed5098e4ab41f59b22dbcbababb1f7282a3a81
Aditya Baddur <adityabaddur@gmail.com>
2023-02-27 21:33:29 +0530

    Test eslint6 changes (#14)
    * updated package-lock json
    
    * updated eslint yml
    
    * changes
    
    * testing
    
    * test
    
    * test
    
    * test
    
    * testing
    
    * testing
    
    * eslint testing
    
    * eslint testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * testing
    
    * testing
    
    * testing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * tetsing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * delete cache
    
    * fixed eslint errors
    
    * cache name
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing laslhdkahskdhlasd
    
    * testing laslhdkahskdhlasd
    
    * testing laslhdkahskdhlasd
    
    * testing
    
    * testing github.event.pull_request.base.ref
    
    * testing github.event.pull_request.base.ref
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    ---------
    
    Co-authored-by: Aditya Baddur <adityabaddur@Adityas-MacBook-Air.local>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RepoListingService } from './services/repo-listing.service';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      providers: [RepoListingService],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   }));
