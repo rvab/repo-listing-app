1dd5a7dcff702331bf1ee77635fc539d6f08ba59
Aditya Baddur <adityabaddur@Adityas-MacBook-Air.local>
2023-03-01 10:20:37 +0530

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,8 +43,6 @@ export class AppComponent {
   =====================================================================*/
 
   async getUserInfo(userName: string) {
-    var a = 10;
-    var b = 100;
     if (this.showSkeleton) return;
 
     this.showSkeleton = true;
