9164ff306abdaa33c59a1bd1b8debad7ed424a39
rvab <>
2023-04-03 16:41:41 +0530

    testing

--- create-release-branch.js
+++ create-release-branch.js
@@ -19,7 +19,7 @@ function setReleaseBranchDate(date) {
   const tzOffset = date.getTimezoneOffset();
   const utcDate = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
   const utcDay = (date.getUTCDay() + 7 - dayOffset) % 7;
-  const utcLastFriday = new Date(utcDate - (utcDay * 24 * 60 * 60 * 1000) - (330 * 60 * 1000));
+  const utcLastFriday = new Date(utcDate - (utcDay * 24 * 60 * 60 * 1000) + (330 * 60 * 1000));
 
   const istTime = utcLastFriday.toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: false });
   console.log('UTC time:', utcLastFriday);
@@ -28,8 +28,8 @@ function setReleaseBranchDate(date) {
 
   console.log('utcLastFriday ---' ,{utcLastFriday})
 
-  releaseBranch.fyle = `app_release_${istTime.slice(0, 10).replaceAll('-', '_')}`;
-  releaseBranch.flow = `flow_app_release_${istTime.slice(0, 10).replaceAll('-', '_')}`;
+  releaseBranch.fyle = `app_release_${istTime.slice(0, 10).replaceAll('/', '_')}`;
+  releaseBranch.flow = `flow_app_release_${istTime.slice(0, 10).replaceAll('/', '_')}`;
 }
 
 
