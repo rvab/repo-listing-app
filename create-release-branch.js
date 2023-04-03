05929fab5982c1a8a54b6057f89d035fbb8cb280
rvab <>
2023-04-03 17:53:06 +0530

    testing

--- create-release-branch.js
+++ create-release-branch.js
@@ -14,16 +14,57 @@ const branchCreationError = {
   flow: ''
 };
 
+// function createReleaseBranch(date) {
+//   const repoPath = "/path/to/your/repo"; // Replace with your actual repo path
+//   const branchName = "new-branch"; // Replace with your desired branch name
+
+//   const commitHash = exec(`git -C ${repoPath} rev-list -1 --before="${utcSixPM.toISOString()}" master`);
+//   commitHash.stdout.on("data", function (hash) {
+//     const command = `git -C ${repoPath} branch ${branchName} ${hash}`;
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`Failed to create branch ${branchName}: ${error}`);
+//       } else {
+//         console.log(`Branch ${branchName} created successfully`);
+//       }
+//     });
+//   });
+// // This code first calculates the current date and time in UTC, and then adds 6 hours to it to get the cutoff time for commits till 6 PM IST. It then uses the git rev-list command to get the hash of the last commit before this cutoff time on the master branch. Finally, it creates a new branch with the specified name at this commit hash using the git branch command.
+
+
+// }
+
+const createReleaseBranch = (environment, releaseBranch, date) => {
+  let branchExists = false;
+  try {
+    execSync(`git ls-remote --exit-code --heads origin ${releaseBranch}`);
+  } catch (error) {
+    branchExists = true;
+  }
+
+  if (!branchExists) {
+    try {
+      const commitHash = execSync(`git rev-list -1 --before="${date.toISOString()}" master`).toString().trim();
+      execSync(`git checkout -b ${releaseBranch} ${commitHash}`);
+      execSync(`git push origin ${releaseBranch}`);
+      console.log(`Branch ${releaseBranch} created successfully.`);
+    } catch (error) {
+      console.error('Branch creation failed with error', JSON.stringify(error));
+      branchCreationError[environment] = JSON.stringify(error);
+    }
+  }
+};
+
+// The below function makes sures that even the script is run manually, then the branch
 function setReleaseBranchDate(date) {
-  const dayOffset = 4; // Friday is the 5th day (0-based index)
+  const dayOffset = 5; // Friday is the 5th day (0-based index)
   const utcDay = (date.getDay() + 7 - dayOffset) % 7;
   const utcLastFriday = new Date(date - (utcDay * 24 * 60 * 60 * 1000));
-  utcLastFriday.setHours(24, 0, 0)
+  utcLastFriday.setHours(18, 0, 0)
 
+  createReleaseBranch('fyle', releaseBranch.fyle, utcLastFriday);
+  createReleaseBranch('flow', releaseBranch.flow, utcLastFriday);
   const [month, currentDate, year] = utcLastFriday.toLocaleString().slice(0,9).split('/')
-  console.log({month, currentDate, year})
-
-  console.log(utcLastFriday.toLocaleString())
   releaseBranch.fyle = `app_release_${year}_${month.length === 1 ? `0${month}`: month}_${currentDate}`;
   releaseBranch.flow = `flow_app_release_${year}_${month.length === 1 ? `0${month}`: month}_${currentDate}`;
 }
@@ -66,29 +107,7 @@ const postMessageToSlack = async () => {
   console.log(`Message sent successfully ${response.statusText}`);
 };
 
-const createReleaseBranch = (environment, releaseBranch) => {
-  let branchExists = false;
-  try {
-    execSync(`git ls-remote --exit-code --heads origin ${releaseBranch}`);
-  } catch (error) {
-    branchExists = true;
-  }
-
-  if (!branchExists) {
-    try {
-      execSync(`git checkout -b ${releaseBranch}`);
-      execSync(`git push origin ${releaseBranch}`);
-      console.log(`Branch ${releaseBranch} created successfully.`);
-    } catch (error) {
-      console.error('Branch creation failed with error', JSON.stringify(error));
-      branchCreationError[environment] = JSON.stringify(error);
-    }
-  }
-};
-
 
 setReleaseBranchDate(new Date());
-createReleaseBranch('fyle', releaseBranch.fyle);
-createReleaseBranch('flow', releaseBranch.flow);
 
-postMessageToSlack(branchCreationError);
+// postMessageToSlack(branchCreationError);
