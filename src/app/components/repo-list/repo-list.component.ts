cba83c09441dc0280dba1ffd9d3f81c70384d766
rvab <>
2024-05-22 09:44:31 +0530

    testing

--- src/app/components/repo-list/repo-list.component.ts
+++ src/app/components/repo-list/repo-list.component.ts
@@ -29,6 +29,7 @@ export class RepoListComponent {
       this.repoList = []
       for (let i = 0 ; i < 10; i++) {
         this.repoList.push(i as any)
+        console.log('coming here-----')
       }
     }
 
